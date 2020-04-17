d5d12e1d977d2f4f368e4394209a2a32a8d1f8e5
Luxumbra27 <luxumbra27@zoho.com>
2020-04-17 08:54:23 +0200

    Change working directory at launch ofr easier use. PEP8 reformat.

--- ffmpeg-split.py
+++ ffmpeg-split.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import csv
-import subprocess
-import math
 import json
+import math
 import os
 import shlex
+import subprocess
 from optparse import OptionParser
 
 
@@ -55,11 +55,11 @@ def split_by_manifest(filename, manifest, vcodec="copy", acodec="copy",
                     filebase = ".".join(filebase.split(".")[:-1])
 
                 split_args += ["-ss", str(split_start), "-t",
-                    str(split_length), filebase + "." + fileext]
+                               str(split_length), filebase + "." + fileext]
                 print "########################################################"
-                print "About to run: "+" ".join(split_cmd+split_args)
+                print "About to run: " + " ".join(split_cmd + split_args)
                 print "########################################################"
-                subprocess.check_output(split_cmd+split_args)
+                subprocess.check_output(split_cmd + split_args)
             except KeyError as e:
                 print "############# Incorrect format ##############"
                 if manifest_type == "json":
@@ -72,16 +72,19 @@ def split_by_manifest(filename, manifest, vcodec="copy", acodec="copy",
                 print e
                 raise SystemExit
 
-def get_video_length(filename):
 
-    output = subprocess.check_output(("ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", filename)).strip()
+def get_video_length(filename):
+    output = subprocess.check_output(("ffprobe", "-v", "error", "-show_entries", "format=duration", "-of",
+                                      "default=noprint_wrappers=1:nokey=1", filename)).strip()
     video_length = int(float(output))
-    print "Video length in seconds: "+str(video_length)
+    print "Video length in seconds: " + str(video_length)
 
     return video_length
 
+
 def ceildiv(a, b):
     return int(math.ceil(a / float(b)))
+
 
 def split_by_seconds(filename, split_length, vcodec="copy", acodec="copy",
                      extra="", video_length=None, **kwargs):
@@ -92,7 +95,7 @@ def split_by_seconds(filename, split_length, vcodec="copy", acodec="copy",
     if not video_length:
         video_length = get_video_length(filename)
     split_count = ceildiv(video_length, split_length)
-    if(split_count == 1):
+    if split_count == 1:
         print "Video length is less then the target split length."
         raise SystemExit
 
@@ -110,83 +113,83 @@ def split_by_seconds(filename, split_length, vcodec="copy", acodec="copy",
             split_start = split_length * n
 
         split_args += ["-ss", str(split_start), "-t", str(split_length),
-                       filebase + "-" + str(n+1) + "-of-" + \
-                        str(split_count) + "." + fileext]
-        print "About to run: "+" ".join(split_cmd+split_args)
-        subprocess.check_output(split_cmd+split_args)
+                       filebase + "-" + str(n + 1) + "-of-" +
+                       str(split_count) + "." + fileext]
+        print "About to run: " + " ".join(split_cmd + split_args)
+        subprocess.check_output(split_cmd + split_args)
 
 
 def main():
     parser = OptionParser()
 
     parser.add_option("-f", "--file",
-                        dest = "filename",
-                        help = "File to split, for example sample.avi",
-                        type = "string",
-                        action = "store"
-                        )
+                      dest="filename",
+                      help="File to split, for example sample.avi",
+                      type="string",
+                      action="store"
+                      )
     parser.add_option("-s", "--split-size",
-                        dest = "split_length",
-                        help = "Split or chunk size in seconds, for example 10",
-                        type = "int",
-                        action = "store"
-                        )
+                      dest="split_length",
+                      help="Split or chunk size in seconds, for example 10",
+                      type="int",
+                      action="store"
+                      )
     parser.add_option("-c", "--split-chunks",
-                        dest = "split_chunks",
-                        help = "Number of chunks to split to",
-                        type = "int",
-                        action = "store"
-                        )
+                      dest="split_chunks",
+                      help="Number of chunks to split to",
+                      type="int",
+                      action="store"
+                      )
     parser.add_option("-S", "--split-filesize",
-                        dest = "split_filesize",
-                        help = "Split or chunk size in bytes (approximate)",
-                        type = "int",
-                        action = "store"
-                        )
+                      dest="split_filesize",
+                      help="Split or chunk size in bytes (approximate)",
+                      type="int",
+                      action="store"
+                      )
     parser.add_option("--filesize-factor",
-                        dest = "filesize_factor",
-                        help = "with --split-filesize, use this factor in time to" \
-                               " size heuristics [default: %default]",
-                        type = "float",
-                        action = "store",
-                        default = 0.95
-                        )
+                      dest="filesize_factor",
+                      help="with --split-filesize, use this factor in time to" 
+                           " size heuristics [default: %default]",
+                      type="float",
+                      action="store",
+                      default=0.95
+                      )
     parser.add_option("--chunk-strategy",
-                        dest = "chunk_strategy",
-                        help = "with --split-filesize, allocate chunks according to" \
-                               " given strategy (eager or even)",
-                        type = "choice",
-                        action = "store",
-                        choices = ['eager', 'even'],
-                        default = 'eager'
-                        )
+                      dest="chunk_strategy",
+                      help="with --split-filesize, allocate chunks according to" 
+                           " given strategy (eager or even)",
+                      type="choice",
+                      action="store",
+                      choices=['eager', 'even'],
+                      default='eager'
+                      )
     parser.add_option("-m", "--manifest",
-                      dest = "manifest",
-                      help = "Split video based on a json manifest file. ",
-                      type = "string",
-                      action = "store"
-                     )
+                      dest="manifest",
+                      help="Split video based on a json manifest file. ",
+                      type="string",
+                      action="store"
+                      )
     parser.add_option("-v", "--vcodec",
-                      dest = "vcodec",
-                      help = "Video codec to use. ",
-                      type = "string",
-                      default = "copy",
-                      action = "store"
-                     )
+                      dest="vcodec",
+                      help="Video codec to use. ",
+                      type="string",
+                      default="copy",
+                      action="store"
+                      )
     parser.add_option("-a", "--acodec",
-                      dest = "acodec",
-                      help = "Audio codec to use. ",
-                      type = "string",
-                      default = "copy",
-                      action = "store"
-                     )
+                      dest="acodec",
+                      help="Audio codec to use. ",
+                      type="string",
+                      default="copy",
+                      action="store"
+                      )
     parser.add_option("-e", "--extra",
-                      dest = "extra",
-                      help = "Extra options for ffmpeg, e.g. '-e -threads 8'. ",
-                      type = "string",
-                      default = "",
-                      action = "store"
-                     )
+                      dest="extra",
+                      help="Extra options for ffmpeg, e.g. '-e -threads 8'. ",
+                      type="string",
+                      default="",
+                      action="store"
+                      )
     (options, args) = parser.parse_args()
 
     def bailout():
@@ -197,7 +200,7 @@ def main():
         bailout()
 
     if options.manifest:
-        split_by_manifest(**(options.__dict__))
+        split_by_manifest(**options.__dict__)
     else:
         video_length = None
         if not options.split_length:
@@ -214,7 +217,10 @@ def main():
                 options.split_length = int(split_filesize / float(file_size) * video_length)
         if not options.split_length:
             bailout()
-        split_by_seconds(video_length=video_length, **(options.__dict__))
+        split_by_seconds(video_length=video_length, **options.__dict__)
+
 
 if __name__ == '__main__':
+    # Change working dir to script location
+    os.chdir(os.path.realpath(os.path.dirname(__file__)))
     main()
